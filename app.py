7cf6304f3c9b382939d8dfdfedffbc54adc6149d
Paulo Matos <plmts@hotmail.com>
2023-09-07 00:02:43 +0000

    criada a função e template para o login

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, flash, redirect, url_for
 
-app = Flask("meu app")
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'blablabla'
 
-posts = [
+posts = [ #tabela em forma de mock
     {
         "titulo": "Minha primeira postagem",
         "texto": "postagem 1"
@@ -21,3 +22,14 @@ def exibir_entradas():
 @app.route('/layout')
 def layout():
     return render_template('layout.html')
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    erro = None
+    if request.method == "POST":
+        if request.form['username'] == 'admin' and request.form['password'] == 'admin':
+            session['logado'] = True
+            flash("Login efetuado com sucesso!")
+            return redirect(url_for('exibir_entradas'))
+        erro = "Usuário ou senha inválido(a)"
+    return render_template('login.html', erro=erro)
