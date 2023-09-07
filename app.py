de4245bac42794bf6ee81427f70f1a39588473f9
Paulo Matos <plmts@hotmail.com>
2023-09-07 01:34:34 +0000

    finalizado com o banco dedados mockado

--- app.py
+++ app.py
@@ -1,22 +1,11 @@
 from flask import Flask, render_template, request, session, flash, redirect, url_for
-
+from posts import posts
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'blablabla'
-
-posts = [ #tabela em forma de mock
-    {
-        "titulo": "Minha primeira postagem",
-        "texto": "postagem 1"
-    },
-    {
-        "titulo":"Minha segunda postagem",
-        "texto":"postagem 2"
-    }
-]
+app.config['SECRET_KEY'] = 'blablabla' #procurar saber sobre
 
 @app.route('/')
 def exibir_entradas():
-    entradas = posts #mock das postagens
+    entradas = posts[::-1] #mock das postagens ordenadas da mais recente para mais antiga
     return render_template('exibir_entradas.html', entradas=entradas) #renderização do html na pasta /templates
 
 @app.route('/layout')
@@ -28,8 +17,33 @@ def login():
     erro = None
     if request.method == "POST":
         if request.form['username'] == 'admin' and request.form['password'] == 'admin':
-            session['logado'] = True
-            flash("Login efetuado com sucesso!")
+            session['logado'] = True #manter usuário logado
+            flash("Login efetuado com sucesso!") #mensagens
             return redirect(url_for('exibir_entradas'))
         erro = "Usuário ou senha inválido(a)"
     return render_template('login.html', erro=erro)
+
+@app.route('/logout')
+def logout():
+    session.pop('logado') #remover session para o logout
+    flash('Logout efetuado com sucesso!')
+    return redirect(url_for('login'))
+
+@app.route('/inserir', methods=['POST'])
+def inserir_entradas(): #rota para inserir novos posts
+    if session['logado']:
+        novo_post= { #criando um novo post para ser publicado
+        "titulo": request.form['titulo'],
+        "texto": request.form['texto']
+        }
+        posts.append(novo_post) #adicionando ao bd mockado
+        flash("Post criado com sucesso!")
+    return redirect(url_for('exibir_entradas'))
+
+@app.route('/posts/<int:id>')
+def exibir_entrada(id):
+    try:
+        entrada = posts[id-1]
+        return render_template('exibir_entrada.html', entrada=entrada)
+    except Exception:
+        return abort(404)
---
+++ posts.py
@@ -0,0 +1,14 @@
+posts = [ #tabela em forma de mock
+    {
+        "titulo": "Minha primeira postagem",
+        "texto": """Lorem ipsum dolor sit amet. Ut culpa excepturi qui saepe repudiandae At quis internos et sint totam et dolores nihil aut internos adipisci. Aut reprehenderit inventore ut magnam iure aut praesentium rerum et blanditiis fuga sit dolorem odit.
+
+Id internos ullam ut unde Quis aut ipsum consequatur. Hic ipsam maxime et repudiandae itaque et quaerat nihil non nihil voluptatem At similique omnis ut odit omnis et enim tenetur.
+
+Sit error accusantium in ducimus quisquam aut dignissimos amet? Non ratione error et enim consequuntur ab beatae dolores ut illum dolorum est unde ullam hic perspiciatis internos quo quisquam expedita."""
+    },
+    {
+        "titulo":"Minha segunda postagem",
+        "texto":"In eius quaerat est beatae dolorum est reprehenderit accusamus ea labore inventore vel voluptatem unde aut voluptas numquam qui velit sint! Et voluptatem minus rem sint eaque qui omnis Quis et quisquam commodi ut nihil odio est quia magni aut commodi perferendis. Est mollitia iure nam error odit cum cupiditate suscipit sit fuga aperiam. Sed accusamus similique hic galisum inventore sed vero debitis aut minus culpa et voluptatum quidem et voluptas omnis. Sit quos rerum quo facere eveniet nam accusamus galisum cum rerum molestiae et eligendi quibusdam. Eos asperiores ipsam ut culpa unde non dolores architecto qui nesciunt veritatis id accusantium odit in obcaecati iste non dignissimos dolorum! Quo blanditiis facilis et corrupti voluptas aut exercitationem perspiciatis in officiis eveniet aut fugiat quisquam."
+    }
+]
